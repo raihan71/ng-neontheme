09d758c26bb3e9ff908e424410d066fd63afebeb
Raihan Nismara <raihannismara@gmail.com>
2024-01-02 20:30:53 +0700

    Fix: Sitemap Placed And Fix Build

--- postbuild.js
+++ postbuild.js
@@ -2,33 +2,52 @@ const exec = require('child_process').exec;
 const fs = require('fs');
 const path = require('path');
 
+var pathPrefix = process.argv.slice(2)[0];
+
+const purgecssConfig = {
+  fontFace: true, // Enable font-face rules
+  keyframes: true, // Enable keyframes rules
+  variables: true, // Enable variable rules
+};
+
 // find the styles css file
-const files = getFilesFromPath('./dist/ngsite-hacker/browser', '.css');
+const files = getAllFiles(`./${pathPrefix}/`, '.css');
 let data = [];
 
 if (!files && files.length <= 0) {
-  console.log("cannot find style files to purge");
+  console.log('cannot find style files to purge');
 }
 
 for (let f of files) {
   // get original file size
-  const originalSize = getFilesizeInKiloBytes('./dist/ngsite-hacker/browser/' + f) + "kb";
-  var o = { "file": f, "originalSize": originalSize, "newSize": "" };
+  const originalSize = getFilesizeInKiloBytes(f) + 'kb';
+  var o = { file: f, originalSize: originalSize, newSize: '' };
   data.push(o);
 }
 
-console.log("Run PurgeCSS...");
+console.log('Run PurgeCSS...');
 
-exec("purgecss -css dist/ngsite-hacker/browser/*.css --content ngsite-hacker/browser/index.html dist/ngsite-hacker/server/*.js -o dist/", function (error, stdout, stderr) {
-  console.log("PurgeCSS done");
+let cmd = [];
+
+for (let d of data) {
+  cmd.push(
+    `npx purgecss --config ${JSON.stringify(purgecssConfig)} -css ${d.file} --content ${pathPrefix}/index.html ${pathPrefix}/*.js -o ${d.file}`
+  );
+}
+
+cmd = cmd.join(' & ');
+
+// console.log(cmd);
+
+exec(cmd, function (error, stdout, stderr) {
+  console.log('PurgeCSS done');
   console.log();
 
   for (let d of data) {
     // get new file size
-    const newSize = getFilesizeInKiloBytes('./dist/ngsite-hacker/browser/' + d.file) + "kb";
+    const newSize = getFilesizeInKiloBytes(d.file) + 'kb';
     d.newSize = newSize;
   }
-
   console.table(data);
 });
 
@@ -38,7 +57,20 @@ function getFilesizeInKiloBytes(filename) {
   return fileSizeInBytes.toFixed(2);
 }
 
-function getFilesFromPath(dir, extension) {
-  let files = fs.readdirSync(dir);
-  return files.filter(e => path.extname(e).toLowerCase() === extension);
+function getAllFiles(dir, extension, arrayOfFiles) {
+  const files = fs.readdirSync(dir);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach(function (file) {
+    if (fs.statSync(dir + '/' + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dir + '/' + file, extension, arrayOfFiles);
+    } else {
+      if (file.endsWith(extension)) {
+        arrayOfFiles.push(path.join(dir, '/', file));
+      }
+    }
+  });
+
+  return arrayOfFiles;
 }
