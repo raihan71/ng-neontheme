b706dbb611b93e178572d93a26dbae84ba6515c9
Raihan Nismara <31585789+raihan71@users.noreply.github.com>
2024-05-09 07:25:50 +0700

    Fix sitemap seo, fix image profile not showing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { NotfoundComponent } from './shared/notfound/notfound.component';
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'home'
+    loadComponent: () =>
+    import('./pages/home/home.component')
+        .then(m => m.HomeComponent)
   },
   {
     path: 'home',
